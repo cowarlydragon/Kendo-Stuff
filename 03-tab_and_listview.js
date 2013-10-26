72d44a33ec05d97aed92bd56883315595167d175
Mueller, Carl - CW <Carl.Mueller@bestbuy.com>
2013-10-26 18:08:47 -0500

    prototype-1 arrdetail no template

---
+++ 03-tab_and_listview.js
@@ -0,0 +1,4 @@
+//Mobile App Initialization Code
+window.drinks = [ "Coffee", "Tea", "Milk", "Orange Juice", "Water", "Soda", "Lemonade", "Sweet Tea", "Espresso", "Hot Chocolate", "Beer", "Wine" ];
+
+var app = new kendo.mobile.Application(document.body);
