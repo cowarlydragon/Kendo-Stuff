1dbabe598011c3dd33338af655a6465b5e37662e
Mueller, Carl - CW <Carl.Mueller@bestbuy.com>
2013-10-26 17:56:32 -0500

    prototype-1 arrdetail no template

---
+++ Proto-1/ArrangerDetail-notemplate.js
@@ -0,0 +1,6 @@
+var app = new kendo.mobile.Application(document.body);
+
+window.drinks = [ "Coffee", "Tea", "Milk", "Orange Juice", "Water", "Soda", "Lemonade", "Sweet Tea", "Espresso", "Hot Chocolate", "Beer", "Wine" ];
+
+
+
